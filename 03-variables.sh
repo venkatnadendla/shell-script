30ba499b4545d2b92b7396e1f695d32dd4bb608d
venkat <ramana.nadela@gmail.com>
2024-12-31 23:23:45 +0530

    conversation script added

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo " please enter user name "  #text entered here will be added as value to the variable
+
+read -s USERNAME #here username is variable name
+echo " user name entered : $USERNAME"
+echo " please entere your password "
+read -s PASSWORD
